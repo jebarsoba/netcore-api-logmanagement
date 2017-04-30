bc67c41374a767b86d37e1075f3a8ee19e2b78d9
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2017-04-30 11:58:06 -0300

    Populating log entry.

--- TodoApi/ErrorHandlingStrategy/LoggingMiddleware.cs
+++ TodoApi/ErrorHandlingStrategy/LoggingMiddleware.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TodoApi.ErrorHandlingStrategy
@@ -18,17 +21,35 @@ namespace TodoApi.ErrorHandlingStrategy
             _logger = logger;
         }
 
-        public Task Invoke(HttpContext context)
+        public async Task Invoke(HttpContext context)
         {
+            Stopwatch stopWatch = Stopwatch.StartNew();
+
             LoggingMiddlewareLogEntry logEntry = new LoggingMiddlewareLogEntry
             {
-                Timestamp = DateTime.Now
+                Timestamp = DateTime.Now,
+                RequestUri = context.Request.Path,
+                RequestMethod = context.Request.Method,
+                RequestThreadId = Thread.CurrentThread.ManagedThreadId,
+                RequestIpAddress = this.GetRequestIpAddress(context),
+                RequestContentType = context.Request.ContentType
             };
 
-            _logger.LogInformation(JsonConvert.SerializeObject(logEntry));
-
             // Call the next delegate/middleware in the pipeline
-            return this._next(context);
+            await _next.Invoke(context);
+
+            logEntry.ResponseStatusCode = context.Response.StatusCode;
+            logEntry.TimeTaken = stopWatch.Elapsed.TotalMilliseconds;
+            logEntry.ResponseContentType = context.Response.ContentType;
+
+            _logger.LogInformation(JsonConvert.SerializeObject(logEntry));
+        }
+
+        private string GetRequestIpAddress(HttpContext context)
+        {
+            IHttpConnectionFeature connection = context.Features.Get<IHttpConnectionFeature>();
+
+            return connection?.RemoteIpAddress?.ToString();
         }
     }
 }
