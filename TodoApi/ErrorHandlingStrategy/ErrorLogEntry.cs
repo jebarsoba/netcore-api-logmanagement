90cd962d98d55a0b1419caa233553ae00cf99e8f
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2018-02-15 10:28:10 -0300

    Adding ErrorLogEntry class.

---
+++ TodoApi/ErrorHandlingStrategy/ErrorLogEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TodoApi.ErrorHandlingStrategy
+{
+    public class ErrorLogEntry
+    {
+        public DateTime? Timestamp { get; set; }
+        public string User { get; set; }
+        public string RequestUri { get; set; }
+        public string RequestMethod { get; set; }
+        public string ExceptionType { get; set; }
+        public string ExceptionMessage { get; set; }
+        public string RequestIpAddress { get; set; }
+        public int RequestThreadId { get; set; }
+        public string RequestContentType { get; set; }
+        public string RequestContentBody { get; set; }
+        public string ExceptionFullMessage { get; set; }
+    }
+}
